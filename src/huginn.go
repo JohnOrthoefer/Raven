86d811ff1c89d32064ec5f246d292b6998e01f8e
John Orthoefer <John@Orthoefer.org>
2019-04-29 11:56:39 -0400

    Added command line args

--- src/huginn.go
+++ src/huginn.go
@@ -2,21 +2,29 @@ package main
 
 import (
   "log"
+  "flag"
   "./raven"
 )
 
-var ConfigFile = "../etc/raven.ini"
+// this will never have true in it.. it's jsut a simple way to hold the main thread open
 var done = make( chan bool)
 
 func main() {
   log.Printf( "Starting...")
-  log.Printf( "Config File: %s", ConfigFile)
-  raven.ReadConfig( ConfigFile)
+
+  configFile := flag.String("config", "../etc/raven.ini", "Configuration File")
+  webPort := flag.String("port", ":8000", "Webserver Port")
+  workers := flag.Int("workers", 3, "Worker Process")
+  flag.Parse()
+
+  log.Printf( "Config File: %s", *configFile)
+  log.Printf( "Listen Port: %s", *webPort)
+  log.Printf( "Workers: %d", *workers)
+  raven.ReadConfig( *configFile)
   raven.BuildSchedule()
   raven.DumpStorage()
   raven.DumpSchedule()
-  raven.StartSchedule( 3)
-  raven.StartWebserver(":8000")
+  raven.StartSchedule( *workers)
+  raven.StartWebserver(*webPort)
   <-done
-  
 }
