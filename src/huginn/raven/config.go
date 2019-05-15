3fbc62381da59f446a32a1cb9d8dddf1b22269e8
John Orthoefer <John@Orthoefer.org>
2019-05-15 13:50:04 -0400

    Changed the ini reader libary

--- src/huginn/raven/config.go
+++ src/huginn/raven/config.go
@@ -23,23 +23,28 @@ import (
 	"./ravenLog"
 	"./ravenTypes"
 	"fmt"
-	ini "github.com/ochinchina/go-ini"
+  "log"
+  "github.com/go-ini/ini"
 )
 
 func makeMap(s *ini.Section) ravenTypes.Kwargs {
 	r := make(ravenTypes.Kwargs)
 	for _, key := range s.Keys() {
 		n := key.Name()
-		v, _ := s.GetValue(n)
+		v := key.Value()
 		r[n] = v
 	}
 	return r
 }
 
 func ReadConfig(iniFile string) {
-	ini := ini.Load(iniFile)
-	for _, section := range ini.Sections() {
-		secName := section.Name
+	cfg, err := ini.Load(iniFile)
+  if err != nil {
+    ravenLog.SendError( 10, "Configuration", fmt.Sprintf("Can not read file %s", iniFile))
+    log.Fatal(err)
+  }
+	for _, section := range cfg.Sections() {
+		secName := section.Name()
 		ravenLog.SendError(10, "Configuration", fmt.Sprintf("Parsing Section %s", secName))
 		keyVal := makeMap(section)
 		AddEntry(secName, keyVal)
