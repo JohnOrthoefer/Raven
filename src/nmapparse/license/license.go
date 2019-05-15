734c2f2f2b3f6dbb06e0922cee2d949c7959b7f7
John Orthoefer <John@Orthoefer.org>
2019-05-15 11:13:01 -0400

    moved Version info into the liscensing software

--- src/nmapparse/license/license.go
+++ src/nmapparse/license/license.go
@@ -25,9 +25,11 @@ import (
 	"os"
 )
 
-func buildLicense() (rtn []string) {
+func buildLicense(v, c string) (rtn []string) {
 	return append(rtn,
 		"Raven Network Discovery and Monitoring",
+		fmt.Sprintf("Version: %s", v),
+		fmt.Sprintf("Commit: %s", c),
 		fmt.Sprintf("Copyright (C) %d  %s\n", 2019,
 			"John{at}Orthoefer{dot}org"),
 		"This program comes with ABSOLUTELY NO WARRANTY.",
@@ -36,21 +38,21 @@ func buildLicense() (rtn []string) {
 }
 
 func LogLicense() {
-	for _, v := range buildLicense() {
+	for _, v := range buildLicense(VERSION, COMMIT) {
 		log.Printf("%s", v)
 	}
 }
 
-func licenseOutput(w io.Writer) {
-	for _, v := range buildLicense() {
+func licenseOutput(w io.Writer, v, c string) {
+	for _, v := range buildLicense(v, c) {
 		fmt.Fprintf(w, "%s", v)
 	}
 }
 
 func PrintLicense() {
-	licenseOutput(os.Stdout)
+	licenseOutput(os.Stdout, VERSION, COMMIT)
 }
 
 func ErrLicense() {
-	licenseOutput(os.Stderr)
+	licenseOutput(os.Stderr, VERSION, COMMIT)
 }
